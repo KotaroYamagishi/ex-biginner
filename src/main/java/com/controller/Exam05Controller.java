f1e6f49c8a0744cd58da8c3451d55a49593f4307
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-06-16 15:36:54 +0900

    add ex5con

--- src/main/java/com/controller/Exam05Controller.java
+++ src/main/java/com/controller/Exam05Controller.java
@@ -1,14 +1,44 @@
 package com.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.domain.Member;
+import com.form.Exam05Form;
+import com.repository.Exam05Repository;
+
 @Controller
+@Transactional
 @RequestMapping("/ex05")
 public class Exam05Controller {
 
+	@ModelAttribute
+	public Exam05Form setUpForm() {
+		return new Exam05Form();
+	}
+	
+	@Autowired
+	private Exam05Repository repository;
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam05";
+	}
+	
+	@RequestMapping("/ex05-result")
+	public String result(Model model,Exam05Form form) {
+		
+		List<Member> memberList;
+		memberList=repository.findByName(form.getName());
+		
+		memberList.forEach(i->System.out.println(i.getName()));
+		model.addAttribute("memberList", memberList);
+		return "exam05-result";
 	}
 }
