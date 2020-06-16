5ce2e10d4f723561dd3f81c04c050277f76810ec
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-06-16 15:39:27 +0900

    add ex05repo

--- src/main/java/com/repository/Exam05Repository.java
+++ src/main/java/com/repository/Exam05Repository.java
@@ -7,29 +7,32 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.domain.Member;
 
 @Repository
-@Component
 public class Exam05Repository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 	
-	public final static RowMapper<Member> MEMBER_ROW_MAPPER
+	private static final RowMapper<Member> MEMBER_ROW_MAPPER
 	=(rs,i)->{
+		System.out.println(i);
 		Member member=new Member();
+		member.setId(rs.getInt("id"));
 		member.setName(rs.getString("name"));
+		member.setAge(rs.getInt("age"));
+		member.setDep_id(rs.getInt("dep_id"));
 		return member;
 	};
 	
 	public List<Member> findByName(String name){
-		String sql="SELECT name FROM members WHERE name LIKE '%:name%'";
-		SqlParameterSource param=new MapSqlParameterSource().addValue("name", name);
-		List<Member> memberList=(List<Member>) template.queryForObject(sql, param, MEMBER_ROW_MAPPER);
+		String sql="SELECT * FROM members WHERE name LIKE :name";
+		SqlParameterSource param=new MapSqlParameterSource().addValue("name","%"+name+"%");
+		List<Member> memberList=template.query(sql, param, MEMBER_ROW_MAPPER);
+		System.out.println(memberList);
 		return memberList;
 	}
 }
