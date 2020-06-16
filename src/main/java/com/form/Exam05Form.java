c85780a364cbc63df51c7f91062a89c43b9ec73a
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2020-06-16 15:38:46 +0900

    add ex05form

---
+++ src/main/java/com/form/Exam05Form.java
@@ -0,0 +1,42 @@
+package com.form;
+
+public class Exam05Form {
+
+	private Integer id;
+	private String name;
+	private Integer age;
+	private Integer dep_id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getDep_id() {
+		return dep_id;
+	}
+
+	public void setDep_id(Integer dep_id) {
+		this.dep_id = dep_id;
+	}
+	
+}
